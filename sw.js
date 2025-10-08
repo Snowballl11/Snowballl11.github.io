c7c95311cc7de76cb9f04f0cf5f67a35958d21ec
Snowballl11 <lpf051114@gmail.com>
2025-10-08 17:06:22 +0800

    Site updated: 2025-10-08 17:06:21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759540666412";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1759914381672";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
